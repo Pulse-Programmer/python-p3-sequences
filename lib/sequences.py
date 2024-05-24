e21c0f541ad58a7595dbfe115df6f7071afab91c
Stephen Ochieng <materiamystic@gmail.com>
2024-05-24 15:11:37 +0300

    Adds fibonacci sequence feature

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [0, 1]
+    
+    if length == 1:
+        fib.pop()
+    
+    elif length == 0:
+        fib.clear()
+   
+    while len(fib) < length:
+       fib.append(fib[-1] + fib[-2])
+    print(fib)
+    return fib
+   
+(print_fibonacci(0))
